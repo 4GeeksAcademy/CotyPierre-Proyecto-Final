967b084cc9d9d5db72a26eae3cc5c6d621c95d96
CotyPierre <135678561+CotyPierre@users.noreply.github.com>
2023-11-25 02:31:49 +0000

    New changes on Navbar, Catalogue and Subcatalog

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,22 +3,24 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<Link to="/catalogo">
-					<span className="btn btn-primary">Catalogo</span>
-				</Link>
-				<Link to="/procedimientos">
-					<span className="btn btn-primary">Procedimientos</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-primary px-2">
+			<Link to={"/"} className="navbar-brand">Gestor de procedimientos</Link>
+			<a className="navbar-brand" href="#"></a>
+			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul className="navbar-nav">
+					<li className="nav-item active">
+						<Link className="nav-link" to={"/"}>Inicio</Link>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Contactos</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Login</a>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
