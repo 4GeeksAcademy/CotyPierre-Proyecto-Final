621f135f2e09be30ca8bfe04e20b6fc86f77d141
CotyPierre <135678561+CotyPierre@users.noreply.github.com>
2023-12-11 07:13:15 +0000

    ultima revision

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-auto py-3 text-center container">
+		<div className="card">
+			<div className="card-body">
+				Enfermería es una profesión noble y vital que se dedica a cuidar a las personas enfermas y necesitadas. Los profesionales de enfermería somos responsables de proporcionar atención médica y emocional a los pacientes, y nuestro trabajo es fundamental para el bienestar de la sociedad.
+				En este sitio, puedes compartir información valiosa sobre técnicas y procedimientos de enfermería que pueden ayudar a los estudiantes de enfermería, los profesionales de enfermería y los pacientes. Algunos temas que podrías cubrir incluyen la administración de medicamentos, la atención de heridas, la higiene personal, técnicas de instrumentación quirúrgica, entre otros.
+			</div>
+		</div>
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
 			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
